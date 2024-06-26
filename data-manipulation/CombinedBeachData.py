0186186f4fca3c78f05acdbbf1912d956d854842
Warren Lam <warrenklam@gmail.com>
2024-06-26 11:08:05 -0500

    Test

--- data-manipulation/CombinedBeachData.py
+++ data-manipulation/CombinedBeachData.py
@@ -3,6 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+#%matplotlib inline
+
+
 # the two data sets we are using
 # https://data.cityofchicago.org/Parks-Recreation/Beach-E-coli-Predictions/xvsz-3xcj/data_preview
 # https://data.cityofchicago.org/Parks-Recreation/Beach-Water-Quality-Automated-Sensors/qmqz-2xku/data_preview
